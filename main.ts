88c34dbe9b5a949c6a4239d0201c67f4469e9354
nono38 <74985866+nono38@users.noreply.github.com>
2021-03-20 18:35:59 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,24 @@
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
+    radio.sendNumber(5)
+})
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
+    radio.sendNumber(5)
+})
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
+    radio.sendNumber(4)
+})
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
+    radio.sendNumber(2)
+})
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
+    radio.sendNumber(5)
+})
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
+    radio.sendNumber(3)
+})
+control.onEvent(EventBusSource.MICROBIT_ID_IO_P15, EventBusValue.MICROBIT_PIN_EVT_FALL, function () {
+    radio.sendNumber(1)
+})
+radio.setGroup(152)
+pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
+pins.setEvents(DigitalPin.P15, PinEventType.Edge)
